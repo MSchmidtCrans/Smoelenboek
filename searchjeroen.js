9f8cb61cbfb1a71aa6abb24b56869507b5825ac6
Jeroen de Reus <48788831+jdereus87@users.noreply.github.com>
2019-04-30 14:29:44 +0200

    testFunctions are working. location/sex works perfectly now

--- searchjeroen.js
+++ searchjeroen.js
@@ -4,12 +4,14 @@ $(document).ready(() => {
         $('.man').show();        
         $('#manbutton').toggleClass('buttonactive');
         $('#vrouwbutton').removeClass('buttonactive');
+        testLocation(); 
     })
     $('#vrouwbutton').on('click', () => {
         $('.man').toggle();
         $('.vrouw').show();
         $('#vrouwbutton').toggleClass('buttonactive');
         $('#manbutton').removeClass('buttonactive');
+        testLocation(); 
     })
 
     $('#groningenbutton').on('click', () => {
@@ -17,18 +19,38 @@ $(document).ready(() => {
         $('.groningen').show();        
         $('#groningenbutton').toggleClass('buttonactive');
         $('#frieslandbutton').removeClass('buttonactive');
-        testActive(); 
+        testSex(); 
     })
     $('#frieslandbutton').on('click', () => {
         $('.groningen').toggle();
         $('.friesland').show();   
         $('#frieslandbutton').toggleClass('buttonactive');
         $('#groningenbutton').removeClass('buttonactive');
+        testSex(); 
     })
 
-    const testActive = function (){
-if ($('#manbutton').hasClass('.buttonactive')){
-    
-}
+// function testSex start
+    const testSex = function (){
+if ($('#manbutton').hasClass('buttonactive')){
+    $('.vrouw').hide()
+};
+if ($('#vrouwbutton').hasClass('buttonactive')){
+    $('.man').hide()
+};
     }; 
+// function testSex end
+
+// function testLocation start
+const testLocation = function (){
+    if ($('#groningenbutton').hasClass('buttonactive')){
+        $('.friesland').hide()
+    };
+    if ($('#frieslandbutton').hasClass('buttonactive')){
+        $('.groningen').hide()
+    };
+        }; 
+    // function testSex end
+
+
+// document ready tag
 });
